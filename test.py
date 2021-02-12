429d8b83bae1c1e322ebd7b53ebe3ffaa52b59ef
Dinesh Udayan <dinesh1599@gmail.com>
2021-02-12 12:53:18 +0530

    take 3

--- test.py
+++ test.py
@@ -6,10 +6,40 @@ import geocoder
 
 #Variable Preset Declaration
 ts = time.time()
-ip = socket.gethostbyname(socket.gethostname())
-g = geocoder.ip('me')
+str_ts = str(ts)
+ts = str_ts.replace('.','')
+# ip = socket.gethostbyname(socket.gethostname())
+# g = geocoder.ip('me')
+# enc = input("Enter the Data to Encrypt!")
 
-print("User IP",ip)
-print("User Timestamp",ts)
-print(g.latlng)
+# print("User IP",ip)
+# print("User Timestamp",ts)
+# print(g.latlng)
+# print('Your Text',enc)
+# print (ts)
+
+
+
+ch = 'Hi Granny'
+listA = []
+final = ''
+keyA = int(ts) % 126
+if(keyA == 0):
+    keyA = (int(ts)*3) % 126
+
+for i in ch:
+    val  = ord(i) +  keyA     #16131070059674842
+    print('ts',int(ts))
+    print(i,':', val , ':', chr(val))
+    final += str(chr(val))
+print(final)
+
+
+  
+# Using chr()+ord() 
+# prints P 
+# x = chr(ord(ch) -51) 
+  
+# print ("The incremented character value is : ",end="") 
+# print (x) 
 
